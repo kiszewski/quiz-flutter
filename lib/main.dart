3c5f38fb121320770dc4b6ca5e8f2ce437f460ea
Leo Kiszewski <kiszewski1999@gmail.com>
2020-07-06 14:43:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() => runApp(PerguntasApp());
+
+
+class PerguntasApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá flutter'),
+      ),
+    );
+  }
+}
