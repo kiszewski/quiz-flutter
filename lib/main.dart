570231e1354c5970e3bb1e1a76a398af3f60616f
Leo Kiszewski <kiszewski1999@gmail.com>
2020-07-06 15:02:49 -0300

    mudança de layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,32 @@ main() => runApp(PerguntasApp());
 
 
 class PerguntasApp extends StatelessWidget {
+  final perguntas = [
+    'Qual sua cor favorita?',
+    'Qual seu animal favorito?'
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá flutter'),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,),
+          ]),
+        ),
       ),
     );
   }
